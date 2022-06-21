fb2bfa386d48700306d0be235424dcef21ebfc20
anurag <anuragpandit271@gmail.com>
2022-06-21 14:16:09 +0530

    maximum subarray sum of size k bruteforce

--- sliding  window problem/maximum_subarrray_sum_size_k.cpp
+++ sliding  window problem/maximum_subarrray_sum_size_k.cpp
@@ -1,26 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-long maximumSubarraySum(vector<int> &Arr,int N, int K){
+//bruteforce
+long maximumSubarraySum(vector<int> &arr,int n, int k){
     long maxi = LONG_MIN;
-        int i=0;
-        int j=0;
-        long sum=0;
-        while(j<N){
-            sum+=Arr[j];
-            if(j-i+1 < K){
-                j++;
-            }
-            else if(j-i+1 == K){
-             maxi = max(maxi,sum);
-             sum-=Arr[i];
-             i++;
-             j++;
-            }
+    long sum;
+
+    for (int i = 0;i<=arr.size()-k;i++){
+        sum = 0;
+        for (int j = i; j < i + k;j++){
+            sum += arr[j];
         }
-        
-        return maxi;
+
+        maxi = max(maxi, sum);
+    }
+
+    return maxi;
 }
+
+
+//optimal approach
+// long maximumSubarraySum(vector<int> &Arr,int N, int K){
+//     long maxi = LONG_MIN;
+//         int i=0;
+//         int j=0;
+//         long sum=0;
+//         while(j<N){
+//             sum+=Arr[j];
+//             if(j-i+1 < K){
+//                 j++;
+//             }
+//             else if(j-i+1 == K){
+//              maxi = max(maxi,sum);
+//              sum-=Arr[i];
+//              i++;
+//              j++;
+//             }
+//         }
+        
+//         return maxi;
+// }
 
 int main(){
     vector<int> nums = {100, 200, 300, 400};
