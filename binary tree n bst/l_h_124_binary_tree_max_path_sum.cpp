786f3ec63bd99787688c011580ec7e3710011db8
anurag <anuragpandit271@gmail.com>
2022-08-01 09:43:55 +0530

    leetcode hard bt maximum path sum|recursion backtracking

---
+++ binary tree n bst/l_h_124_binary_tree_max_path_sum.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode{
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+
+// Function to create the Binary Tree
+struct TreeNode* create()
+{
+    int data;
+    struct TreeNode* tree;
+ 
+    // Dynamically allocating memory
+    // for the tree-node
+    tree = new TreeNode;
+ 
+    cout << "\nEnter data to be inserted "
+         << "or type -1 for no insertion : ";
+ 
+    // Input from the user
+    cin >> data;
+ 
+    // Termination Condition
+    if (data == -1)
+        return nullptr;
+ 
+    // Assign value from user into tree
+    tree->val = data;
+ 
+    // Recursively Call to create the
+    // left and the right sub tree
+    cout << "Enter left child of : "
+         << data;
+    tree->left = create();
+ 
+    cout << "Enter right child of : "
+         << data;
+    tree->right = create();
+ 
+    // Return the created Tree
+    return tree;
+};
+
+
+    int maxPath(TreeNode* node,int &maxi){
+        if(node==nullptr) return 0;
+        
+        int leftSum = max(0,maxPath(node->left,maxi));
+        int rightSum = max(0,maxPath(node->right,maxi));
+        
+        maxi = max(maxi,node->val+leftSum+rightSum);
+        
+        return node->val + max(leftSum,rightSum);
+    }
+    
+    int maxPathSum(TreeNode* root) {
+        
+        int maxi = INT_MIN;
+        maxPath(root,maxi);
+        return maxi;
+    }
+
+
+int main(){
+
+    TreeNode *root = create();
+
+    cout<<maxPathSum(root) << endl;
+}
