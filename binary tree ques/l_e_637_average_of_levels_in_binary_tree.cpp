4c534424970aba896b4b1b8eb5029a719d8c5ed9
anurag <anuragpandit271@gmail.com>
2022-09-02 09:55:27 +0530

    leetcode easy 637 average of levels in binary tree| level order traversal

---
+++ binary tree ques/l_e_637_average_of_levels_in_binary_tree.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode{
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+};
+
+
+// Function to create the Binary Tree
+struct TreeNode* create()
+{
+    int data;
+    struct TreeNode* tree;
+ 
+    // Dynamically allocating memory
+    // for the tree-node
+    tree = new TreeNode;
+ 
+    cout << "\nEnter data to be inserted "
+         << "or type -1 for no insertion : ";
+ 
+    // Input from the user
+    cin >> data;
+ 
+    // Termination Condition
+    if (data == -1)
+        return nullptr;
+ 
+    // Assign value from user into tree
+    tree->val = data;
+ 
+    // Recursively Call to create the
+    // left and the right sub tree
+    cout << "Enter left child of : "
+         << data;
+    tree->left = create();
+ 
+    cout << "Enter right child of : "
+         << data;
+    tree->right = create();
+ 
+    // Return the created Tree
+    return tree;
+};
+
+
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> ans;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while(!q.empty()){
+            int size = q.size();
+            double avg=0;
+            
+            for(int i=0;i<size;i++){
+               TreeNode* cur = q.front();
+                q.pop();
+                avg+=cur->val;
+                if(cur->left) q.push(cur->left);
+                if(cur->right) q.push(cur->right);
+            }
+            
+            ans.push_back(avg/size);
+        }
+        
+        return ans;
+    }
+
+
+
+    int main(){
+        TreeNode *root = create();
+        vector<double> ans = averageOfLevels(root);
+
+        for(auto it:ans){
+            cout << it << " ";
+        }
+    }
+
+
