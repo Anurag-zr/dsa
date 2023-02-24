00d77b5880162e7a5aeac78e27f924401aa0fb00
anurag <anuragpandit271@gmail.com>
2023-02-24 18:44:23 +0530

    leetcode hard 1675 minimum deviation in array|priority queue and constructive thinking

---
+++ Heap,priority_queue/l_h_1675_minimize_deviation_in_array.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    int minimumDeviation(vector<int>& nums) {
+        int ans = 1e9;
+        int mini = 1e9;
+        
+        priority_queue<int> pq;
+        
+        for(auto n: nums){
+            if(n%2) n = n*2;
+            pq.push(n);
+            mini = min(mini,n);
+        }
+        
+        while(pq.top()%2==0){
+            ans = min(ans,pq.top()-mini);
+            mini = min(mini,pq.top()/2);
+            pq.push(pq.top()/2);
+            pq.pop();
+        }
+        
+        return min(ans,pq.top()-mini);
+    }
+
+
+int main(){
+
+    vector<int> nums = {4,1,5,20,3};
+    cout << minimumDeviation(nums) << endl;
+}
